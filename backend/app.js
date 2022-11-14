54c5b933c874810426b7bb81cfa2573f27842538
Numero 3 <notnumero3@gmail.com>
2022-11-14 15:24:13 +0100

    adding mongoDb

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
+// Required Import
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+// MongoDB Connexion
+mongoose
+  .connect(
+    `mongodb+srv://${username}:${password}@piiquante-cluster.b0o2sqj.mongodb.net/?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+ // Parse request into json
 app.use(express.json());
 
 app.use((req, res, next) => {

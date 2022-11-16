8243cf899d627aea92a034a3a313e00087f5a8e2
Numero 3 <notnumero3@gmail.com>
2022-11-16 18:16:05 +0100

    Refact

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,13 +1,18 @@
 const jwt = require('jsonwebtoken');
  
+// Middleware : Auth
 module.exports = (req, res, next) => {
    try {
+    /* Get token from the headers request, split it to only keep the token */
        const token = req.headers.authorization.split(' ')[1];
-       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    /* Decode token with 'verify' method (token + passphrase) */
+       const decodedToken = jwt.verify(token, 'N3FXM_OCDEV_PIIQUANTE');
+    /* Get userID from the decoded Token */
        const userId = decodedToken.userId;
+    /* Pass auth object to the request to auth navigation */
        req.auth = { userId };
 	next();
-   } catch(error) {
-       res.status(401).json({ error });
+   } catch(err) {
+       res.status(401).json({err});
    }
 };

0b27adc6b41e438b10d84688b619ce22964eb04b
Numero 3 <notnumero3@gmail.com>
2022-11-16 23:14:31 +0100

    Adding createSauce and getOneSauce routes

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,5 +1,15 @@
 const Sauce = require('../models/Sauce');
 
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then((sauce) => {
+        res.status(200).json(sauce);
+      })
+      .catch((error) => {
+        res.status(404).json({ error: error });
+      });
+  };
+
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
       .then((sauce) => {
@@ -9,3 +19,20 @@ exports.getAllSauces = (req, res, next) => {
         res.status(400).json(err);
       });
   };
+
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+  });
+  sauce.save()
+    .then(() => {
+      res.status(201).json({ message: "Sauce " + req.body.sauce + " ajouté" });
+    })
+    .catch((err) => {
+      res.status(400).json({ error: err });
+    });
+};

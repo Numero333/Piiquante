38fd3f71e8cd23234b7410e343ad78c2d812ab38
Numero 3 <notnumero3@gmail.com>
2022-11-16 17:58:09 +0100

    Refact

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,17 +7,17 @@ const User = require("../models/user");
 
 // Sign Up Fonction : 
 exports.signup = (req, res, next) => {
-  // Using 'bcrypt' to encrypt (10 times) password
+  /* Using 'bcrypt' to encrypt (10 times) password */
   bcrypt
     .hash(req.body.password, 10)
-    // Take mail in the request + encrypted password
-    // and create a new user
+    /* Take mail in the request + encrypted password
+       and create a new user */
     .then((hash) => {
       const user = new User({
         email: req.body.email,
         password: hash,
       });
-      // Saving new user to the database
+      /* Saving new user to the database */
       user
         .save()
         .then(() => res.status(201).json({ message: "New user !" }))
@@ -28,8 +28,8 @@ exports.signup = (req, res, next) => {
 
 // Login Fonction :
 exports.login = (req, res, next) => {
-  // Using express method 'findOne' to verify that the user
-  // is existing in the database
+  /* Using express method 'findOne' to verify that the user
+     is existing in the database */
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
@@ -37,8 +37,8 @@ exports.login = (req, res, next) => {
           message: "Invalid : Username/password"
         });
       }
-      // Using bcrypt method 'compare' to compare password
-      // from the client request to the database
+      /* Using bcrypt method 'compare' to compare password
+         from the client request to the database */
       bcrypt
         .compare(req.body.password, user.password)
         .then((valid) => {
@@ -49,13 +49,13 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            // Using 'jsonwebtoken' to attribute a Token valid for 24h
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+            /* Using 'jsonwebtoken' to attribute a Token valid for 24h */
+            token: jwt.sign({ userId: user._id }, "N3FXM_OCDEV_PIIQUANTE", {
               expiresIn: "24h",
             }),
           });
         })
-        .catch((error) => res.status(401).json({ error }));
+        .catch((err) => res.status(401).json({err}));
     })
-    .catch((error) => res.status(401).json({ error }));
+    .catch((err) => res.status(401).json({err}));
 };

df94a184f0279b166f623ab46ac339ac24275d92
Numero 3 <notnumero3@gmail.com>
2022-11-16 22:57:53 +0100

    Update

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
-// Required Import
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/user");
+const sauceRoutes = require("./routes/sauce");
+
+dotenv.config();
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
 
 const app = express();
 
@@ -11,13 +17,19 @@ mongoose
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch(() => console.log("Connexion à MongoDB échouée !"));
+  .catch((err) => console.log("Connexion à MongoDB échouée ! " + err.message));
 
  // Parse request into json
 app.use(express.json());
 
 app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    next();
+  });
 
-});
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", sauceRoutes);
 
 module.exports = app;

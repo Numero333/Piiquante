7eff96589616fd36a7a4fd9a62399723578c68f3
Numero 3 <notnumero3@gmail.com>
2022-11-17 10:37:33 +0100

    Adding delete et modify controls

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/Sauce');
+const fs = require('fs');
 
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
@@ -34,5 +35,56 @@ exports.createSauce = (req, res, next) => {
     })
     .catch((err) => {
       res.status(400).json({ error: err });
+    });
+};
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then((sauce) => {
+        const filename = sauce.imageUrl.split("/images/")[1];
+        if (sauce.userId !== req.auth.userId) {
+          res.status(403).json({ error: "Authentification error" });
+        } else {
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({ _id: req.params.id })
+            .then(() => {
+              res.status(200).json({ message: "Supprimé!" + filename });
+            })
+            .catch((err) => {
+              res.status(400).json({ error: err });
+            });
+        });
+      }
+      })
+      .catch((error) => res.status(500).json({ error }));
+  };
+
+  exports.modifySauce = (req, res, next) => {
+    const sauceModify = req.file
+        ? {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get("host")}/images/${
+            req.file.filename
+            }`,
+        }
+        : { ...req.body };
+
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      const filename = sauce.imageUrl.split("/images/")[1];
+      if (sauce.userId !== req.auth.userId) {
+        res.status(403).json({ error: "Authentification error" });
+      } else {
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.updateOne(
+            { _id: req.params.id },
+            { ...sauceModify, _id: req.params.id }
+          )
+            .then(() =>
+              res.status(201).json({ message: "Sauce updated successfully!" })
+            )
+            .catch((err) => res.status(400).json({error: err}));
+        });
+      }
     });
 };

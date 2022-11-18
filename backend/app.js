c6e2c3624ad9b4951181baaf2d354d78db3ab8cf
Numero 3 <notnumero3@gmail.com>
2022-11-18 22:57:14 +0100

    Refact

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const userRoutes = require("./routes/user");
-const sauceRoutes = require("./routes/sauce");
 const path = require("path");
 
+// Router
+const userRoutes = require("./routes/user");
+const sauceRoutes = require("./routes/sauce");
+
+// Environment variables
+const dotenv = require("dotenv");
 dotenv.config();
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 
+// 
 const app = express();
 
 // MongoDB Connexion
@@ -23,6 +27,7 @@ mongoose
  // Parse request into json
 app.use(express.json());
 
+// CORS config
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
@@ -30,8 +35,10 @@ app.use((req, res, next) => {
     next();
   });
 
+// Static folder for images
 app.use("/images", express.static(path.join(__dirname, "images")));
 
+// Router of user & sauce routes
 app.use("/api/auth", userRoutes);
 app.use("/api/sauces", sauceRoutes);
 
